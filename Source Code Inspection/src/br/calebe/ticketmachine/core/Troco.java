58d2d6cd2f0e19d59dd144eefa49fc7c867b946d
GabrielBueno200 <gabrielbueno200@gmail.com>
2022-09-17 20:18:11 -0300

    refatoração do construtor de Troco

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -12,36 +12,20 @@ class Troco {
 
     public Troco(int valor) {
         papeisMoeda = new PapelMoeda[6];
-        int count = 0;
-        while (valor % 100 != 0) {
-            count++;
+
+        int[] valoresPapelModa = { 2, 5, 10, 20, 50, 100 };
+        int valorPapelMoeda, count;
+
+        for (int i = valoresPapelModa.length - 1; i <= 0; i--) {
+            count = 0;
+            valorPapelMoeda = valoresPapelModa[i];
+
+            while (valor % valorPapelMoeda != 0) {
+                count++;
+            }
+
+            papeisMoeda[i] = new PapelMoeda(valorPapelMoeda, count);
         }
-        papeisMoeda[5] = new PapelMoeda(100, count);
-        count = 0;
-        while (valor % 50 != 0) {
-            count++;
-        }
-        papeisMoeda[4] = new PapelMoeda(50, count);
-        count = 0;
-        while (valor % 20 != 0) {
-            count++;
-        }
-        papeisMoeda[3] = new PapelMoeda(20, count);
-        count = 0;
-        while (valor % 10 != 0) {
-            count++;
-        }
-        papeisMoeda[2] = new PapelMoeda(10, count);
-        count = 0;
-        while (valor % 5 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(5, count);
-        count = 0;
-        while (valor % 2 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(2, count);
     }
 
     public Iterator<PapelMoeda> getIterator() {
