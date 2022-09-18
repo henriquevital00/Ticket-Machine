0249ae11b3e2e8e9f56a614f970068e42e85d576
GabrielBueno200 <gabrielbueno200@gmail.com>
2022-09-18 19:48:57 -0300

    consertada contagem de notas de 50 #9

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -21,10 +21,12 @@ class Troco {
         papeisMoeda[5] = new PapelMoeda(100, count);
 
         count = 0;
-        while (valor % 50 != 0) {
+        while (valor / 50 > 0) {
             count++;
+            valor -= 50;
         }
         papeisMoeda[4] = new PapelMoeda(50, count);
+
         count = 0;
         while (valor % 20 != 0) {
             count++;
