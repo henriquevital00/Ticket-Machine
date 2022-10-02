2db8fad7f1c0f93a64deb4b7dee0556189fc68fc
GabrielBueno200 <gabrielbueno200@gmail.com>
2022-10-01 23:47:49 -0300

    added TicketMachine tests

---
+++ Source Code Inspection/src/test/java/br/calebe/ticketmachine/core/TicketMachineTest.java
@@ -0,0 +1,68 @@
+package br.calebe.ticketmachine.core;
+
+import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
+import br.calebe.ticketmachine.exception.SaldoInsuficienteException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mockito;
+
+public class TicketMachineTest {
+    
+    @ParameterizedTest
+    @ValueSource(ints = {-2, -5, -10, -20, -50, -100})
+    public void Inserir_DeveLancarExcecaoPapelMoedaInvalida_QuandoInformarPepelMoedaNegativa() 
+    {
+        var sut = new TicketMachine(Mockito.anyInt());
+        
+        var thrown  = Assertions.catchThrowable(() -> sut.inserir(-2));
+
+        Assertions.assertThat(thrown).isInstanceOf(PapelMoedaInvalidaException.class);
+    }
+    
+    @ParameterizedTest
+    @CsvSource({
+        "2, 5",
+        "5, 10",
+        "10, 20"
+    })
+    public void Imprimir_DeveLancarExcecaoSaldoInsuficiente_QuandoSaldoMenorQueValor(int quantia, int valor) throws PapelMoedaInvalidaException 
+    {
+        var sut = new TicketMachine(valor);
+
+        sut.inserir(quantia);
+        var thrown  = Assertions.catchThrowable(() -> sut.imprimir());
+
+        Assertions.assertThat(thrown).isInstanceOf(SaldoInsuficienteException.class);
+    }
+    
+    @ParameterizedTest
+    @ValueSource(ints = {2, 5, 10, 20, 50, 100})
+    public void Imprimir_DeveObterSaldoSemCasasDecimais(int quantia) throws SaldoInsuficienteException, PapelMoedaInvalidaException 
+    {
+        var sut = new TicketMachine(0);
+
+        sut.inserir(quantia);
+        var resultSaldo = sut.imprimir();
+        
+        var expectedResult = "*****************\n";
+        expectedResult += "*** R$ " + sut.getSaldo() + " ****\n";
+        expectedResult += "*****************\n";
+        Assertions.assertThat(resultSaldo).isEqualTo(expectedResult);
+    }
+    
+    @ParameterizedTest
+    @ValueSource(ints = {2, 5, 10, 20, 50, 100})
+    public void DeveAtualizarERetornarSaldo(int quantia) throws PapelMoedaInvalidaException, SaldoInsuficienteException
+    {
+        var valorBilhete = 2;
+        var sut = new TicketMachine(valorBilhete);
+        
+        sut.inserir(quantia);
+        sut.imprimir();
+        
+        
+        Assertions.assertThat(sut.getSaldo()).isEqualTo(quantia - valorBilhete);
+    }
+}
